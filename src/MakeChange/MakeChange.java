66c0d98fb4a73a552d10cfa7e7f1826c6ba8f933
Allan Meyer <ameyer409@yahoo.com>
2023-11-17 18:25:34 -0500

    test message from class

--- src/MakeChange/MakeChange.java
+++ src/MakeChange/MakeChange.java
@@ -1,10 +1,35 @@
+/*
+In the cash register we will calculate the amount of change returned to a customer 
+based on the purchase price and the amount tendered. We will also notify the 
+attendant how many of each piece of currency 
+($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to make the change for the customer. 
+Change should be provided using the largest bill and coin denominations as possible. 
+Denominations that are not used should not be displayed.
+*/
+
 package MakeChange;
+
+import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		double amount = 0.0, tendered = 0.0;
+		
+		//Part 1: The user is prompted asking for the price of the item.
+		System.out.print("What is the price of the item you wish to purchase: ");
+		amount = kb.nextDouble();
 
+		//Part 2: The user is then prompted asking how much 
+		//		  money was tendered by the customer.
+		System.out.print("What is the price of the item you wish to purchase: ");
+		tendered = kb.nextDouble();
+		
+		System.out.println(amount);
+		System.out.println(tendered);
+		
+		kb.close();
 	}
 
 }
